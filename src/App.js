8a3f7fd698d6cebef1bc0b5df128c3729af5ef30
Zak Laughton <zlaughton@atlassian.com>
2021-04-23 10:28:46 -0500

    commit initial tutorial work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Canvas, useThree, extend, useFrame } from "react-three-fiber";
+import { a, useSpring } from "react-spring/three";
 
+import "./App.css";
+
+extend({ OrbitControls });
+
+function Cube(props) {
+  const [isBig, setIsBig] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const ref = useRef();
+
+  useFrame(() => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+  });
+
+  const { size, x } = useSpring({
+    size: isBig ? [2, 2, 2] : [1, 1, 1],
+    x: isBig ? 2 : 0,
+  });
+
+  const color = isHovered ? "pink" : "salmon";
+
+  return (
+    <a.mesh
+      {...props}
+      ref={ref}
+      scale={size}
+      position-x={x}
+      onClick={() => setIsBig(!isBig)}
+      onPointerOut={() => setIsHovered(false)}
+      onPointerOver={() => setIsHovered(true)}
+    >
+      <cylinderBufferGeometry attach='geometry' args={[1, 1, 3, 12]} />
+      <meshStandardMaterial attach='material' color={color} />
+    </a.mesh>
+  );
+}
+// args = [width, height, depth]
+
+function Scene() {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+  return (
+    <>
+      <ambientLight />
+      <pointLight intensity={0.3} position={[-1, 2, 4]} />
+      <Cube rotation={[10, 10, 0]} position={[0, 0, 0]} />
+      <Cube rotation={[10, 20, 0]} position={[2, 2, 0]} />
+      <orbitControls args={[camera, domElement]} />
+    </>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <Scene />
+    </Canvas>
   );
 }
 
