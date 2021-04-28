95acf1856c3c9570c351c544318c5e3b6e590953
Zak Laughton <zlaughton@atlassian.com>
2021-04-28 13:51:14 -0500

    committing up to "Lights and shadows" video

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import React, { useState, useRef } from "react";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { Canvas, useThree, extend, useFrame } from "react-three-fiber";
 import { a, useSpring } from "react-spring/three";
@@ -30,35 +30,57 @@ function Cube(props) {
       ref={ref}
       scale={size}
       position-x={x}
+      castShadow={true}
+      receiveShadow={true}
       onClick={() => setIsBig(!isBig)}
       onPointerOut={() => setIsHovered(false)}
       onPointerOver={() => setIsHovered(true)}
     >
-      <cylinderBufferGeometry attach='geometry' args={[1, 1, 3, 12]} />
-      <meshStandardMaterial attach='material' color={color} />
+      <sphereBufferGeometry attach='geometry' args={[1, 8, 6]} />
+      <meshPhongMaterial
+        flatShading={true}
+        roughness={1}
+        metalness={0.5}
+        shininess={100}
+        attach='material'
+        color={color}
+      />
     </a.mesh>
   );
 }
-// args = [width, height, depth]
+// box args = [width, height, depth ]
+// sphere args = [width, height, depth ]
+
+function Plane() {
+  return (
+    <mesh receiveShadow={true} rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, -5]}>
+      <planeBufferGeometry attach='geometry' args={[20, 20]} />
+      <meshPhongMaterial attach='material' color='#D3D3D3' />
+    </mesh>
+  );
+}
 
 function Scene() {
   const {
     camera,
     gl: { domElement },
   } = useThree();
+
   return (
     <>
       <ambientLight />
-      <pointLight intensity={0.3} position={[-1, 2, 4]} />
+      <spotLight castShadow={true} intensity={0.6} position={[0, 10, 4]} />
       <Cube rotation={[10, 10, 0]} position={[0, 0, 0]} />
       <Cube rotation={[10, 20, 0]} position={[2, 2, 0]} />
+      <Plane />
       <orbitControls args={[camera, domElement]} />
     </>
   );
 }
+
 function App() {
   return (
-    <Canvas>
+    <Canvas shadowMap={true}>
       <Scene />
     </Canvas>
   );
